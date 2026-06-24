6d4ac22ca9620fce05b253ffec7aff42de8e54fa
Morhaf Ghziel <ghzielmorhaf@gmail.com>
2026-06-24 21:28:48 +0300

    new update

--- components/language-switch.tsx
+++ components/language-switch.tsx
@@ -32,7 +32,7 @@ const LanguageSwitch = () => {
         </span>
       </div>
 
-      {/* AR side */}
+      {/* AR sie */}
       <div
         className="relative z-10 h-full flex items-center justify-center px-4"
       >
